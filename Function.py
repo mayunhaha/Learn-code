a14282545ef9b82f541855acb4a011ce4dfcea90
请叫我老范 <1096409605@qq.com>
2024-01-10 18:14:47 +0800

    创建

---
+++ Function.py
@@ -0,0 +1,17 @@
+from operation import Operation
+
+a = 8
+b = 9
+r = Operation().addition(a, b)
+
+t = Operation().subtraction(a, b)
+y = Operation().division(a, b)
+u = Operation().multiplication(a, b)
+print(r)
+print(t)
+print(y)
+print(u)
+
+
+
+
---
+++ operation.py
@@ -0,0 +1,21 @@
+class Operation():
+    # 四则运算
+    def addition(self, a, b):
+        # 加法
+        return a + b
+
+    def subtraction(self, a, b):
+        # 减法
+        return a - b
+
+    def division(self, a, b):
+        # 除法
+        return a / b
+
+    def multiplication(self, a, b):
+        # 乘法
+        return (a * b)
+
+
+
+
